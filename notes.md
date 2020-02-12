8b9a3749e681ba7f74a74891dc03bd340a81edf6
leannethng <leanne.thng@gmail.com>
2020-02-12 11:20:50 -0800

    moved search component into app file for now
    Need to figure out how to access state from one component into another, I belive this is through lifting state.

--- notes.md
+++ notes.md
@@ -4,3 +4,5 @@
 - `onChange()` Fires when there’s a change in any of the form’s input elements, each keystroke
 - Can not use `console.log(event.key);` to see key presses
 - To see what key we use we need to use `onKeyPress()`
+
+- I split out my components into different files but i need to access the state form one into another, have just added the search intot he base app for now and will figure out how to lift up state.
--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, {Component} from 'react'
 import Header from './components/Header'
-import Search from './components/Search'
+// import Search from './components/Search'
 
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = {
+      // default state, can't be updated
+      searchTerm:''
+    }
+  }
+  // With create react app , we can write methods inside component as arrow functions instead of using constructor and bind
+  handleChange = event => {
+    // target is the element it, value is what is in the input
+    // const value = event.target.value 
+    // console.log(event.target.value)
+    // can use this way of writing as value is repeated
+    // console.log(event.target.key);
+      const {value} = event.target;
+      // by setting the search term in the state and using that on the input as the value we have created a controlled input. 
+      this.setState((prevState, props) => ({
+        // take the old props and spread them out here
+        ...prevState,
+        // then we overwrite the ones we want
+        searchTerm: value
+      }));
+      
+      if (value.length > 2){
+        console.log('This is a valid search term');
+        return true;
+      }
+    };
+   
+   handleKeyPress = event => {
+    const {value} = event.target;
+    console.log(value);
+    if (value.length > 2 && event.key === 'Enter'){
+      console.log(`Search for ${value}`);
+      
+    }
+    // console.log(event.key);
+  }
 
+
+
+
+  render() {
+    const { searchTerm } = this.state;
     return(
       <div className="page">
         <Header />
-        <Search />
+        <div className="search grid">
+          {/* Stack of gif images */}
+
+          {/* Input field */}
+          <input
+            className="input grid-item"
+            placeholder="Type Something"
+            // run a function
+            onChange={this.handleChange}
+            // Grabbing info about what key is pressed, we are interested in enter
+            onKeyPress={this.handleKeyPress}
+            // Using the value in the state
+            value={searchTerm}
+            />
+        </div>
+      <h1>{searchTerm}</h1>
       </div>
     )
-    };
-  }
+  };
+}
 
 export default App;
